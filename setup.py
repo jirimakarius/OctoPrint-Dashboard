3c148941af8678ac236b564a30c550adf63ac999
Jiří Makarius <meadowfrey@gmail.com>
2017-03-12 16:41:28 +0100

    rearranged backend folder architecture

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='octoprint_dashboard',
+    version='0.1',
+    description='Runs server application for controlling multiple 3D printers',
+    author='Jiří Makarius',
+    packages=['octoprint_dashboard'],
+    include_package_data=True,
+    zip_safe=False,
+    url='',
+    install_requires=[
+        'Flask'
+    ]
+)
